9b6bc3182a192e58c9d20d6ac71e1e0724e20bd4
Brendan C <brendan@cooney.tech>
2019-07-02 20:19:28 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+new Vue({
+    el: '#app',
+    data: {
+
+        health:100,
+        ended: false
+
+    },
+
+    methods:{
+
+        punch: function(){
+            this.health -= Math.random()*20;
+            if(this.health <= 0){
+                this.ended = true
+            }
+        },
+        
+        restart: function(){
+            this.health = 100
+            this.ended = false
+        }
+
+
+    },
+
+    computed: {
+
+
+
+    }
+
+});
